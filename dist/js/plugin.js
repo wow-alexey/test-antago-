64bfa0e09a6c5446482f41033f27e70b4a761bdc
Alexey Dmytrenko <wow-alexey@mail.ru>
2017-04-28 23:27:10 +0300

    third commit. Js start

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,3 +1,69 @@
 ;(function () {
+    var btn = $('.btn');
+    var textarea = $('.textarea');
+    var error = $('.error');
+
+    function check(e) {
+        e.preventDefault();
+        var textareaContent = textarea.val();
+
+        if (textareaContent == '') {
+            textarea.addClass('error-border');
+            error.addClass('show');
+
+        } else {
+            textarea.removeClass('error-border');
+            error.removeClass('show');
+            console.log(textareaContent);
+        }
+        textarea.focus(function () {
+            textarea.removeClass('error-border');
+            error.removeClass('show');
+        });
+    }
+
+    btn.click(check);
+
+    textarea.keydown(function (e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            var textareaContent = textarea.val();
+
+            if (textareaContent == '') {
+                textarea.addClass('error-border');
+                error.addClass('show');
+            } else {
+                textarea.removeClass('error-border');
+                error.removeClass('show');
+                console.log(textareaContent);
+            }
+            textarea.focus(function () {
+                textarea.removeClass('error-border');
+                error.removeClass('show');
+            });
+        }
+    });
 
 })(jQuery);
+
+;(function getDate() {
+    var date = new Date();
+    var options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    console.log(date.toLocaleString("ru", options));
+
+})(jQuery);
+
+// ;(function () {
+//     var btn = $('.btn');
+//     btn.keydown(function (e) {
+//
+//         if (e.ctrlKey && e.keyCode == 13) {
+//             // Ctrl-Enter pressed
+//         }
+//     });
+//
+// })(jQuery);
+
