a15f9aabba545dbc19d176666849f319be54f193
Alexey Dmytrenko <wow-alexey@mail.ru>
2017-04-30 17:54:04 +0300

    Final result

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -3,73 +3,56 @@
     var textarea = $('.textarea');
     var error = $('.error');
     var comments = $('.user-comments');
-    var reply = $('.reply');
+    var message = $('.message');
     var recalls = $('.recalls');
+
+    function getDate() {
+        var date = new Date();
+        var options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+
+        var newDate = date.toLocaleString("ru", options);
+        var res = newDate.substring(0, newDate.length - 3);
+        return res;
+    };
+
+    textarea.focus(function () {
+        textarea.removeClass('error-border');
+        error.removeClass('show');
+    });
 
     function check(e) {
         e.preventDefault();
         var textareaContent = textarea.val();
-
+        var content = '<div class="message">' +
+                            '<div class="user-information">' +
+                                '<h5 class="user-name">Имя пользователя</h5>' +
+                                '<div class="message-day">' + getDate() + '</div>' +
+                            '</div>' +
+                            '<div class="text-message">' + textareaContent + '</div>' +
+                        '</div>';
         if (textareaContent == '') {
             textarea.addClass('error-border');
             error.addClass('show');
-
         } else {
-            comments.append('<div class="reply">' +
-                                '<h5 class="user-name">Имя пользователя</h5>' +
-                                '<div class="full-date">'+ getDate() +'</div>' +
-                                '<div class="comment-content">'+ textareaContent +'</div>' +
-                            '</div>');
-            (function clean() {
-                textarea.val('');
-            })();
+            comments.append(content);
+            textarea.val('');
             console.log(textareaContent);
         }
-        textarea.focus(function () {
-            textarea.removeClass('error-border');
-            error.removeClass('show');
-        });
     }
 
-    btn.click(check);
+    btn.on('click', check);
 
     textarea.keydown(function (e) {
         if (e.ctrlKey && e.keyCode == 13) {
-            var textareaContent = textarea.val();
-
-            if (textareaContent == '') {
-                textarea.addClass('error-border');
-                error.addClass('show');
-            } else {
-                comments.append('<div class="reply">' +
-                                    '<h5 class="user-name">Имя пользователя</h5>' +
-                                    '<div class="full-date">'+ getDate() +'</div>' +
-                                    '<div class="comment-content">'+ textareaContent +'</div>' +
-                                '</div>');
-                (function clean() {
-                    textarea.val('');
-                })();
-                console.log(textareaContent);
-            }
-            textarea.focus(function () {
-                textarea.removeClass('error-border');
-                error.removeClass('show');
-            });
+            check(e);
         }
     });
 
 })(jQuery);
 
-function getDate() {
-    var date = new Date();
-    var options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
 
-    var newDate = date.toLocaleString("ru", options);
-    var res = newDate.substring(0, newDate.length - 3);
-    return res;
-};
 
